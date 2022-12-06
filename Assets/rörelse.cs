a0672eafd7b1f68fbe55e600c8aefa53b23b01b2
Mxtern <max.toresjo@elev.ga.lbs.se>
2022-12-06 10:21:09 +0100

    unity game

---
+++ Assets/rörelse.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rörelse : MonoBehaviour
+{
+    [SerializeField]
+    KeyCode up;
+    [SerializeField]
+    KeyCode down;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        transform.position = new Vector3(transform.position.x, Mathf.Clamp(transform.position.y, -3, 3), 0);
+       
+        if (Input.GetKey(up))
+        {
+            
+            transform.position += new Vector3(0, 5, 0)* Time.deltaTime;
+        }
+
+        if (Input.GetKey(down))
+        {
+            
+            transform.position += new Vector3(0, -5, 0) * Time.deltaTime;
+        }
+    }
+}
